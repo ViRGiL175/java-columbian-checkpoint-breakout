7b1be1be7b12e73292723b004b98799cb1737e28
ViRGiL7 <virgil7g@gmail.com>
2017-03-04 01:19:38 +0400

    Garage graphic and logic, waites for graphic

--- src/main/java/rape/brutal/virgil/breakout/BreakoutGame.java
+++ src/main/java/rape/brutal/virgil/breakout/BreakoutGame.java
@@ -4,12 +4,12 @@ import com.googlecode.lanterna.TerminalPosition;
 import com.googlecode.lanterna.screen.TerminalScreen;
 import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
 import com.googlecode.lanterna.terminal.Terminal;
-import rape.brutal.virgil.breakout.graphic.ATSGraphic;
-import rape.brutal.virgil.breakout.graphic.AverageCarGraphic;
-import rape.brutal.virgil.breakout.graphic.CarGraphic;
-import rape.brutal.virgil.breakout.graphic.CheckPointGraphic;
+import rape.brutal.virgil.breakout.graphic.*;
 
 import java.io.IOException;
+import java.util.Random;
+
+import static java.lang.Thread.sleep;
 
 /**
  * Created by ViRGiL7 on 03.03.2017.
@@ -25,6 +25,7 @@ public class BreakoutGame {
     private ATSGraphic ats3Graphic;
     private CarGraphic carGraphic;
     private CheckPointGraphic checkPointGraphic;
+    private GarageGraphic garageGraphic;
 
     public BreakoutGame() {
         try {
@@ -58,9 +59,41 @@ public class BreakoutGame {
         ats2Graphic = new ATSGraphic("ATS2", terminalScreen);
         ats3Graphic = new ATSGraphic("ATS3", terminalScreen);
         checkPointGraphic = new CheckPointGraphic("Columbian Checkpoint", terminalScreen);
-        carGraphic = new AverageCarGraphic(DEFAULT_CAR_POSITION, "Semyon and Family",
-                terminalScreen);
-        drawBattleScreen(terminalScreen);
+        garageGraphic = new GarageGraphic("Bandits Garage", terminalScreen);
+
+        createNewCar(terminalScreen);
+
+        driveCarToCheckpoint(terminalScreen);
+    }
+
+    private void driveCarToCheckpoint(TerminalScreen terminalScreen) throws IOException {
+        for (int i = 0; i < 42; i++) {
+            TerminalPosition position = new TerminalPosition(carGraphic.getPosition().getColumn() + 1,
+                    carGraphic.getPosition().getRow());
+            carGraphic.setPosition(position);
+            drawBattleScreen(terminalScreen);
+        }
+    }
+
+    private void createNewCar(TerminalScreen terminalScreen) throws IOException {
+        Random random = new Random();
+        if (random.nextBoolean()) {
+            carGraphic = new BigCarGraphic(DEFAULT_CAR_POSITION, "Semyon and Family", terminalScreen);
+        } else if (random.nextBoolean()) {
+            carGraphic = new BattleCarGraphic(DEFAULT_CAR_POSITION, "Semyon and Family", terminalScreen);
+        } else {
+            carGraphic = new AverageCarGraphic(DEFAULT_CAR_POSITION, "Semyon and Family", terminalScreen);
+        }
+
+        if (carGraphic.getClass() != AverageCarGraphic.class) {
+            garageGraphic.setClosed(false);
+            drawBattleScreen(terminalScreen);
+            shortWait();
+            drawBattleScreen(terminalScreen);
+            garageGraphic.setClosed(true);
+            drawBattleScreen(terminalScreen);
+            shortWait();
+        }
     }
 
     private void mainMenu(TerminalScreen terminalScreen) {
@@ -69,19 +102,24 @@ public class BreakoutGame {
 
     private void drawBattleScreen(TerminalScreen terminalScreen) throws IOException {
 
-        ats1Graphic.draw(new TerminalPosition(6, 1));
+        garageGraphic.draw(new TerminalPosition(2, 6));
 
-        ats2Graphic.draw(new TerminalPosition(21, 4));
+        ats1Graphic.draw(new TerminalPosition(16, 1));
+
+        ats2Graphic.draw(new TerminalPosition(28, 4));
 
         ats3Graphic.draw(new TerminalPosition(44, 2));
 
         terminalScreen.newTextGraphics().drawLine(55, 0, 55, 6, '#');
 
-        terminalScreen.newTextGraphics().drawLine(0, 10, DEFAULT_CAR_POSITION.getColumn(), 10, '.');
-
-        carGraphic.draw(carGraphic.getPosition());
-
-        terminalScreen.newTextGraphics().drawLine(DEFAULT_CAR_POSITION.getColumn() + 6, 10, 53, 10, '.');
+        if (carGraphic != null) {
+            terminalScreen.newTextGraphics().drawLine(0, 10, carGraphic.getPosition().getColumn(), 10, '.');
+            carGraphic.draw(carGraphic.getPosition());
+            terminalScreen.newTextGraphics().drawLine(carGraphic.getPosition().getColumn() + 6, 10, 53, 10, '.');
+        } else {
+            terminalScreen.newTextGraphics().drawLine(0, 10, DEFAULT_CAR_POSITION.getColumn(), 10, '.');
+            terminalScreen.newTextGraphics().drawLine(DEFAULT_CAR_POSITION.getColumn() + 6, 10, 53, 10, '.');
+        }
 
         terminalScreen.newTextGraphics().drawLine(57, 10, terminalScreen.getTerminalSize().getColumns(), 10, '.');
 
@@ -93,5 +131,25 @@ public class BreakoutGame {
 
         terminalScreen.newTextGraphics().drawLine(57, 13, terminalScreen.getTerminalSize().getColumns(), 13, '.');
 
+        terminalScreen.refresh();
+
+        tinyWait();
+
+    }
+
+    private void tinyWait() {
+        try {
+            sleep(200);
+        } catch (InterruptedException var1) {
+            var1.printStackTrace();
+        }
+    }
+
+    private void shortWait() {
+        try {
+            sleep(500);
+        } catch (InterruptedException var1) {
+            var1.printStackTrace();
+        }
     }
 }
---
+++ src/main/java/rape/brutal/virgil/breakout/graphic/GarageGraphic.java
@@ -0,0 +1,41 @@
+package rape.brutal.virgil.breakout.graphic;
+
+import com.googlecode.lanterna.TerminalPosition;
+import com.googlecode.lanterna.screen.TerminalScreen;
+
+import java.io.IOException;
+
+public class GarageGraphic extends ASCIIGraphic {
+
+    private boolean isClosed = false;
+
+    public GarageGraphic(String name, TerminalScreen terminalScreen) {
+        super(name, terminalScreen);
+    }
+
+    public void setClosed(boolean closed) {
+        isClosed = closed;
+    }
+
+    public void draw(TerminalPosition cursorPosition) throws IOException {
+        newTextGraphics().putString(0, 0, "+");
+        newTextGraphics().drawLine(1, 0, 9, 0, '-');
+        newTextGraphics().putString(10, 0, "+");
+        newTextGraphics().drawLine(10, 1, 10, 2, '|');
+        newTextGraphics().putString(0, 3, "+");
+        newTextGraphics().drawLine(1, 3, 9, 3, '-');
+        newTextGraphics().putString(10, 3, "+");
+        newTextGraphics().drawLine(0, 1, 0, 2, '|');
+        newTextGraphics().putString(1, 1, "El Garage");
+
+        if (isClosed) {
+            newTextGraphics().drawLine(1, 2, 9, 2, '#');
+        } else {
+            newTextGraphics().putString(1, 2, "|");
+            newTextGraphics().putString(9, 2, "|");
+        }
+
+        terminalScreen.newTextGraphics().drawImage(cursorPosition, basicTextImage);
+    }
+
+}
